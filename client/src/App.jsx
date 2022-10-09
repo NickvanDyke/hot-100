817b3a5ea4b99998ce47432ceeb9ed98e5c0b3c0
Nick van Dyke <nickjvandyke15@gmail.com>
2022-10-09 12:48:40 -0500

    Add box shadow transition

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,26 @@ import { Top100Chart } from './Top100Chart'
 import { apollo } from './lib/apollo.js'
 import { Box, Stack, Typography } from '@mui/material'
 import { Account } from './Account'
+import { useEffect, useRef, useState } from 'react'
 
 export const App = () => {
+	const [onTop, setOnTop] = useState(true)
+	const listRef = useRef()
+	useEffect(() => {
+		if (!listRef) return
+		const cb = () => setOnTop(listRef.current.scrollTop === 0)
+		listRef.current.addEventListener('scroll', cb)
+		return () => {
+			listRef.current?.removeEventListener('scroll', cb)
+		}
+	}, [setOnTop, listRef])
+
 	return (
 		<ApolloProvider client={apollo}>
 			<Stack height='100%'>
 				<Stack
-					boxShadow={4}
+					boxShadow={onTop ? 0 : 4}
+					sx={{ transition: 'box-shadow 0.3s' }}
 					direction='row'
 					justifyContent='space-between'>
 					<Typography
@@ -20,7 +33,10 @@ export const App = () => {
 					</Typography>
 					<Account />
 				</Stack>
-				<Box height='100%' overflow='scroll'>
+				<Box
+					height='100%'
+					overflow='scroll'
+					ref={listRef}>
 					<Top100Chart />
 				</Box>
 			</Stack>
